69c8d433d80b4bd44096de525660c273dd5661b9
acupoftee <teedia31@gmail.com>
2025-11-29 15:02:48 -0500

    update default client and server routes

--- config/config-example.js
+++ config/config-example.js
@@ -20,7 +20,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: 'steamgamessacredgold.up.railway.app',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
