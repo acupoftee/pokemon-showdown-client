d3b83280449272880fd40377fce1bbe86f122f28
acupoftee <teedia31@gmail.com>
2025-11-30 18:59:17 -0500

    use correct server domain serverd on psim.us

--- config/config-example.js
+++ config/config-example.js
@@ -20,7 +20,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'steamgamessacredgold.up.railway.app',
+	host: 'steamgamessacredgold-up-railway-app.psim.us',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
