efde912d2879a8ea50d2ac192d944cbcbecafaea
acupoftee <teedia31@gmail.com>
2025-12-03 22:42:55 -0500

    Use DNS resolvable domain, not reverse proxied domain

--- config/config-example.js
+++ config/config-example.js
@@ -20,7 +20,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'steamgamessacredgold-up-railway-app.psim.us',
+	host: 'steamgamessacredgold.up.railway.app',
 	port: 443,
 	httpport: 8080,
 	altport: 80,
